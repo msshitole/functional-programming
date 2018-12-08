59ee28c8ababb4cb53d0ede9729bed65746ef1a1
ms_shitole <shitole@prime-research.com>
2018-12-08 21:47:28 +0100

    implemented the chapt2 examples

--- src/main/java/functional/programming/in/action/chapt1/Chapt2.java
+++ src/main/java/functional/programming/in/action/chapt1/Chapt2.java
@@ -1,7 +1,9 @@
 package functional.programming.in.action.chapt1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * - Coping with changing requirements
@@ -49,7 +51,55 @@ public class Chapt2 {
         heavyApples = filterApples(inventory, "", 150, false);
         System.out.println(heavyApples);
 
+        System.out.println("\nFourth attempt : : Behavior parameterization");
+        System.out.println("==============================================");
+        greenApples = filterApples(inventory, new GreenApplePredicate());
+        System.out.println("Green apples :: ");
+        System.out.println(greenApples);
+        heavyApples = filterApples(inventory, new HeavyApplePredicate());
+        System.out.println(heavyApples);
+
+        System.out.println("\nTackling verbocity");
+        System.out.println("Sixth attempt, use anonymous classes");
+        System.out.println("========================================");
+        List<Apple> redAndHeavyApples = filterApples(inventory, new ApplePredicate() {
+            @Override
+            public boolean test(Apple apple) {
+                return "red".equalsIgnoreCase(apple.getColor())
+                        && apple.getWeight() >= 150;
+            }
+        });
+
+        System.out.println("Red and heavy apples :: ");
+        System.out.println(redAndHeavyApples);
+
+        System.out.println("\nOne step further : Tackling verbocity with clean and concise code");
+        System.out.println("The magic of Lambdas");
+        System.out.println("=================================================================");
+        System.out.println("Green apples :: ");
+        greenApples = filterApples(inventory, apple -> "green".equalsIgnoreCase(apple.getColor()));
+        System.out.println(greenApples);
+        redAndHeavyApples = filterApples(inventory, apple -> "red".equalsIgnoreCase(apple.getColor()) && apple.getWeight() >= 150);
+        System.out.println("Red and heavy apples :: ");
+        System.out.println(redAndHeavyApples);
+
+        System.out.println("\nThe Seventh attempt :: More abstraction, abstracting inventory list");
+        System.out.println("=====================================================================");
+        greenApples = filter(inventory, apple -> "green".equalsIgnoreCase(apple.getColor()));
+        System.out.println("Green Apples :: ");
+        System.out.println("-----------------");
+        System.out.println(greenApples);
+
+        System.out.println("\nEven Numbers :: ");
+        System.out.println("----------------------");
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+
+        List<Integer> evenNumbers = filter(numbers, number -> number % 2 == 0);
+        System.out.println(evenNumbers);
+
+
     }
+
 
     /**
      * First attempt: filtering green apples
@@ -112,6 +162,55 @@ public class Chapt2 {
             if((flag && color.equalsIgnoreCase(apple.getColor())
             || (!flag && apple.getWeight() >= weight))) {
                 result.add(apple);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Fourth attempt : Abstract the filtering criteria
+     */
+    public static interface ApplePredicate {
+        boolean test(Apple apple);
+    }
+
+    public static class GreenApplePredicate implements ApplePredicate {
+
+        @Override
+        public boolean test(Apple apple) {
+            return "green".equalsIgnoreCase(apple.getColor());
+        }
+    }
+
+    public static class HeavyApplePredicate implements ApplePredicate {
+        @Override
+        public boolean test(Apple apple) {
+            return apple.getWeight() >= 150;
+        }
+    }
+
+    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate predicate) {
+        List<Apple> result = new ArrayList<>();
+
+        for (Apple apple : inventory) {
+            if(predicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * More abstraction : Generic and flexible
+     */
+    public static <T> List<T> filter(List<T> inventory, Predicate<T> predicate) {
+        List<T> result = new ArrayList<>();
+
+        for (T element : inventory ) {
+            if(predicate.test(element)) {
+                result.add(element);
             }
         }
 
